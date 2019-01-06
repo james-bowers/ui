e65114507da2ba64711a4f6fc5da0ed7141f14a1
James Bowers <james@james-bowers.me>
2019-01-06 17:43:00 +0000

    add example page to show off components

---
+++ example/example.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import Button from "../components/Button";
+
+const Example = () => <Button>Example</Button>;
+
+ReactDOM.render(<Example />, document.getElementById("app"));
